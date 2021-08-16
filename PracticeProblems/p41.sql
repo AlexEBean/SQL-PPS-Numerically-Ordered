2dfbf24e68004f3ede8fa8613436f807998030ef
Alex <alexbean95@gmail.com>
2021-08-16 09:05:29 -0600

    Reviewed p problems 38, 39, 41, 42, 52, and 56

--- PracticeProblems/p41.sql
+++ PracticeProblems/p41.sql
@@ -1,4 +1,4 @@
 SELECT OrderID, DATE(OrderDate) AS OrderDate, DATE(RequiredDate) AS RequiredDate, DATE(ShippedDate) AS ShippedDate
 	FROM orders
-    WHERE DATE(ShippedDate) > DATE(RequiredDate)
+    WHERE DATE(ShippedDate) >= DATE(RequiredDate)
     ORDER BY OrderID;
