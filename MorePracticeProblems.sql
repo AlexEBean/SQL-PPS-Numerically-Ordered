8e508b9b0ba25307002a1f61dae799f967ab3430
Alex <alexbean95@gmail.com>
2021-08-23 10:51:46 -0600

    Moved answers into two different SQL files for easier review

---
+++ MorePracticeProblems.sql
@@ -0,0 +1,200 @@
+-- 1
+
+SELECT ProductID, COUNT(StandardCost) AS TotalPriceChanges 
+	FROM ProductCostHistory
+    GROUP BY ProductID;
+
+-- 2
+
+SELECT CustomerID, COUNT(SalesOrderID) AS TotalOrders
+	FROM SalesOrderHeader
+    GROUP BY CustomerID
+    ORDER BY TotalOrders DESC, CustomerID;
+
+-- 3
+
+SELECT ProductID, DATE(MIN(OrderDate)) AS FirstOrder, DATE(MAX(OrderDate)) AS LastOrder
+	FROM SalesOrderDetail d
+    JOIN SalesOrderHeader h
+		ON d.SalesOrderID = h.SalesOrderID
+	GROUP BY ProductID
+    ORDER BY ProductID;
+
+-- 4
+
+SELECT p.ProductID, ProductName, DATE(MIN(h.OrderDate)) AS FirstOrder, DATE(MAX(h.OrderDate)) AS LastOrder
+	FROM product p
+    JOIN SalesOrderDetail d
+		ON d.ProductID = p.ProductID
+	JOIN SalesOrderHeader h 
+		ON d.SalesOrderID = h.SalesOrderID
+	GROUP BY ProductID
+    ORDER BY ProductID;
+
+-- 5
+
+SELECT ProductID, StandardCost
+	FROM ProductCostHistory
+    WHERE '2012-04-15'
+    BETWEEN StartDate 
+		AND EndDate
+    ORDER BY ProductID;
+
+-- 6
+
+SELECT ProductID, StandardCost
+	FROM ProductCostHistory
+    WHERE IF (
+				EndDate IS NULL, 
+                StandardCost,
+                '2014-04-15' BETWEEN StartDate AND EndDate
+			)
+	GROUP BY ProductID
+    ORDER BY ProductID;
+
+-- 7
+
+SELECT DATE_FORMAT(StartDate, '%Y/%m') AS ProductListPriceMonth, COUNT(DATE_FORMAT(StartDate, '%Y/%m')) AS TotalRows
+	FROM ProductListPriceHistory
+    GROUP BY DATE_FORMAT(StartDate, '%Y/%m');
+
+-- 9
+
+SELECT ProductID, ListPrice
+	FROM ProductListPriceHistory
+    WHERE EndDate IS NULL
+    ORDER BY ProductID;
+
+-- 10
+
+SELECT p.ProductID, p.ProductName
+	FROM product p
+    LEFT JOIN ProductListPriceHistory  plph
+		ON p.ProductID = plph.ProductID
+	WHERE plph.ProductID IS NULL
+    ORDER BY p.ProductID;
+
+-- 12
+
+SELECT ProductID
+	FROM ProductListPriceHistory
+    WHERE EndDate IS NULL
+    GROUP BY ProductID
+    HAVING COUNT(*) > 1
+    ORDER BY ProductID;
+
+-- 13
+
+SELECT p.ProductID, ProductName, ProductSubCategoryName, DATE(MIN(h.OrderDate)) AS FirstOrder, DATE(MAX(h.OrderDate)) AS LastOrder
+	FROM product p
+    LEFT JOIN SalesOrderDetail d
+		ON d.ProductID = p.ProductID
+	LEFT JOIN SalesOrderHeader h 
+		ON d.SalesOrderID = h.SalesOrderID
+	LEFT JOIN ProductSubCategory psc
+		ON p.ProductSubcategoryID = psc.ProductSubcategoryID
+	GROUP BY ProductName
+    ORDER BY ProductName;
+
+-- 14
+
+SELECT p.ProductID, ProductName, p.ListPrice AS Prod_ListPrice, plph.ListPrice AS PriceHist_LatestListPrice, p.ListPrice - plph.ListPrice AS Diff
+	FROM product p
+    JOIN ProductListPriceHistory plph
+		ON p.ProductID = plph.ProductID
+    WHERE p.ListPrice != plph.ListPrice AND plph.EndDate IS NULL;
+
+-- 15
+
+SELECT p.ProductID, DATE(OrderDate) AS OrderDate, ProductName, OrderQty AS Qty, DATE(SellStartDate) AS SellStartDate, DATE(SellEndDate) AS SellEndDate
+	FROM product p
+    JOIN SalesOrderDetail d
+		ON p.ProductID = d.ProductID
+	JOIN SalesOrderHeader h
+		ON d.SalesOrderID = h.SalesOrderID
+	WHERE OrderDate < SellStartDate OR SellEndDate < OrderDate
+    ORDER BY p.ProductID, OrderDate;
+
+-- 16
+
+SELECT p.ProductID, DATE(OrderDate) AS OrderDate, OrderQty AS Qty, DATE(SellStartDate) AS SellStartDate, DATE(SellEndDate) AS SellEndDate, 
+	CASE
+		WHEN OrderDate < SellStartDate
+        THEN 'Sold before start date'
+        ELSE 'Sold after end date'
+    END AS ProblemType
+	FROM product p
+    JOIN SalesOrderDetail d
+		ON p.ProductID = d.ProductID
+	JOIN SalesOrderHeader h
+		ON d.SalesOrderID = h.SalesOrderID
+	WHERE OrderDate < SellStartDate OR SellEndDate < OrderDate
+    ORDER BY p.ProductID, OrderDate;
+
+-- 18
+
+Select
+ Product.ProductID
+ ,ProductName
+ ,ProductSubCategoryName
+ ,Date(Min(OrderDate)) as FirstOrder
+ ,Date(Max(OrderDate)) as LastOrder
+From Product
+ Left Join SalesOrderDetail Detail
+ on Product.ProductID = Detail.ProductID
+ Left Join SalesOrderHeader Header
+ on Header.SalesOrderID = Detail .SalesOrderID
+ Left Join ProductSubCategory 
+ on ProductSubCategory .ProductSubCategoryID = Product.ProductSubCategoryID
+Where
+ Color = 'Silver'
+Group by
+ Product.ProductID
+ ,ProductName
+ ,ProductSubCategoryName
+Order by LastOrder desc;
+
+-- The column name of color was coded as 'Color' in the WHERE Statement
+-- It shouldn't be written as a string.
+
+-- 20
+
+SELECT c.CustomerID, CONCAT(c.FirstName, ' ', c.LastName) AS CustomerName, COUNT(DISTINCT soh.SalesPersonEmployeeID) AS TotalDifferentSalesPeople
+	FROM customer c
+    JOIN SalesOrderHeader soh
+		ON c.CustomerID = soh.CustomerID
+	GROUP By c.CustomerID
+	HAVING COUNT(DISTINCT soh.SalesPersonEmployeeID) > 1
+    ORDER BY CustomerName;
+
+-- 21
+
+Select
+ Customer.CustomerID
+ ,FirstName + ' ' + LastName as CustomerName
+ ,OrderDate
+ ,SalesOrderHeader.SalesOrderID 
+ ,SalesOrderDetail.ProductID
+ ,Product.ProductName
+ ,LineTotal
+From SalesOrderDetail
+ Join Product
+ on Product.ProductID = SalesOrderDetail .ProductID
+ Join SalesOrderHeader
+ on SalesOrderHeader .SalesOrderID = SalesOrderDetail .SalesOrderID
+ Join Customer
+ on Customer.CustomerID = SalesOrderHeader.CustomerID
+Order by
+ CustomerID
+ ,OrderDate
+Limit 100;
+
+-- In line 9, SalesOrderHeader changed to SalesOrderDetail
+-- In line 12, SalesOrderDetail changed to SalesOrderHeader
+
+-- 22
+
+SELECT ProductName
+	FROM product
+    GROUP BY ProductName
+    HAVING COUNT(ProductName) > 1;
---
+++ PracticeProblems.sql
@@ -0,0 +1,331 @@
+-- 1
+
+SELECT * 
+    FROM Shippers;
+
+-- 2
+
+SELECT CategoryName, Description 
+	FROM Categories;
+
+-- 3
+
+SELECT FirstName, LastName, HireDate
+	FROM employees
+    WHERE Title = 'Sales Representative';
+
+-- 4
+
+SELECT FirstName, LastName, HireDate
+	FROM employees
+    WHERE Title = 'Sales Representative' AND COUNTRY = 'USA';
+
+-- 5
+
+SELECT OrderID, OrderDate
+	FROM orders
+    WHERE EmployeeID = 5;
+
+-- 6
+
+SELECT *
+	FROM suppliers
+    WHERE NOT ContactTitle = 'Marketing Manager';
+
+-- 7
+
+SELECT ProductID, ProductName
+	FROM products
+    WHERE ProductName LIKE '%queso%';
+
+-- 8
+
+SELECT OrderID, CustomerID, ShipCountry
+	FROM orders
+    WHERE ShipCountry = 'France' OR ShipCountry = 'Belgium';
+
+-- 9
+
+SELECT OrderID, CustomerID, ShipCountry
+	FROM orders
+    WHERE ShipCountry IN ('Brazil', 'Mexico', 'Argentina', 'Venezuela');
+
+-- 10
+
+SELECT FirstName, LastName, Title, BirthDate
+	FROM employees
+    ORDER BY BirthDate;
+
+-- 11
+
+SELECT FirstName, LastName, Title, CONVERT(BirthDate, DATE) AS DateOnlyBirthDate
+	FROM employees
+    ORDER BY BirthDate;
+
+-- 12
+
+SELECT FirstName, LastName, CONCAT(FirstName, ' ', LastName) AS FullName
+	FROM employees;
+
+-- 13
+
+SELECT OrderID, ProductID, UnitPrice, Quantity
+	FROM OrderDetails
+    ORDER BY OrderId, ProductID;
+
+-- 14
+
+SELECT COUNT(CustomerID) AS TotalCustomers
+	FROM customers;
+
+-- 15
+
+SELECT MIN(OrderDate) AS FirstOrder
+	FROM Orders;
+
+-- 16
+
+SELECT DISTINCT Country
+	FROM customers
+    ORDER BY Country;
+
+-- 17
+
+SELECT ContactTitle, COUNT(ContactTitle) AS TotalContactTitle
+	FROM customers
+    GROUP BY ContactTitle
+    ORDER BY TotalContactTitle DESC;
+
+-- 18
+
+SELECT p.ProductID, p.ProductName, s.CompanyName AS Supplier
+	FROM Products p
+    JOIN Suppliers s
+        ON p.SupplierID = s.SupplierID;
+
+-- 19
+
+SELECT o.OrderID, o.OrderDate, s.CompanyName AS Supplier
+	FROM Orders o
+    JOIN Shippers s
+        ON o.ShipVia = s.ShipperID
+    WHERE OrderID < 10270
+    ORDER BY OrderID;
+
+-- 20
+
+SELECT c.CategoryName, COUNT(p.CategoryID) AS TotalProducts
+	FROM Categories c
+    JOIN Products p
+        ON c.CategoryID = p.CategoryID
+    GROUP BY p.CategoryID
+    ORDER BY TotalProducts DESC;
+
+-- 21
+
+SELECT Country, City, COUNT(Country AND City) AS TotalCustomers
+	FROM customers
+    GROUP BY Country, City
+    ORDER BY TotalCustomers DESC;
+
+-- 22
+
+SELECT productID, ProductName, UnitsInStock, ReorderLevel
+	FROM products
+    WHERE UnitsInStock <= ReorderLevel
+    ORDER BY ProductID;
+
+-- 23
+
+SELECT productID, ProductName, UnitsInStock, UnitsOnOrder, ReorderLevel, Discontinued
+	FROM products
+    WHERE UnitsInStock + UnitsOnOrder <= ReorderLevel AND Discontinued = 0;
+
+-- 24
+
+SELECT CustomerID, CompanyName, Region
+	FROM customers
+    ORDER BY Region IS NULL, region, CustomerID;
+
+-- 25
+
+SELECT ShipCountry, AVG(Freight) AS AverageFreight
+	FROM orders
+    GROUP BY ShipCountry
+    ORDER BY AverageFreight DESC
+    LIMIT 3;
+
+-- 26
+
+SELECT ShipCountry, AVG(CASE WHEN OrderDate LIKE '2015%' THEN Freight END) AS AverageFreight
+	FROM orders
+    GROUP BY ShipCountry
+    ORDER BY AverageFreight DESC
+    LIMIT 3;
+
+-- 27
+
+10806
+
+
+-- This code helped me find it.
+SELECT
+ OrderID, OrderDate, ShipCountry
+FROM Orders
+WHERE OrderDate LIKE '2015%' AND OrderDate NOT BETWEEN '2015-01-01' AND '2015-12-31'
+ORDER BY ShipCountry
+
+-- 28
+
+SELECT ShipCountry, AVG(CASE WHEN OrderDate BETWEEN '2015-05-06 18:00:00' AND '2016-05-06 18:00:00' THEN Freight END) AS AverageFreight
+	FROM orders
+    GROUP BY ShipCountry
+    ORDER BY AverageFreight DESC
+    LIMIT 3;
+
+-- 29
+
+SELECT e.EmployeeID, e.LastName, o.OrderID, p.ProductName, od.Quantity
+	FROM Employees e
+    JOIN Orders o
+		ON e.EmployeeID = o.EmployeeID
+	JOIN OrderDetails od
+		ON o.OrderID = od.OrderID
+	JOIN Products p
+		ON od.ProductID = p.ProductID
+	ORDER BY o.OrderID, p.ProductID;
+
+-- 30
+
+SELECT c.CustomerID AS Customers_CustomerID, o.CustomerID AS Orders_CustomerID 
+	FROM Customers c
+    LEFT JOIN Orders o
+    ON c.CustomerID = o.CustomerID
+    WHERE o.CustomerID IS NULL;
+
+-- 31
+
+SELECT DISTINCT CustomerID 
+	FROM customers
+    WHERE CustomerID  
+		NOT IN
+        (SELECT CustomerID 
+			FROM orders
+			WHERE EmployeeID = 4);
+
+-- 32
+
+SELECT c.CustomerID, c.CompanyName, o.OrderID, SUM(od.unitPrice * od.Quantity) AS TotalOrderAmount
+	FROM Customers c
+    JOIN Orders o
+        ON c.CustomerID = o.CustomerID
+    JOIN OrderDetails od
+        ON o.OrderID = od.OrderID
+    WHERE OrderDate LIKE '2016%'
+    GROUP BY o.OrderID
+    HAVING (TotalOrderAmount >= 10000)
+    ORDER BY TotalOrderAmount DESC;
+
+-- 33
+
+SELECT c.CustomerID, c.CompanyName, o.OrderID, SUM(od.unitPrice * od.Quantity) AS TotalOrderAmount
+	FROM Customers c
+    JOIN Orders o
+        ON c.CustomerID = o.CustomerID
+    JOIN OrderDetails od
+        ON o.OrderID = od.OrderID
+    WHERE OrderDate LIKE '2016%'
+    GROUP BY c.CustomerID
+    HAVING (TotalOrderAmount >= 15000)
+    ORDER BY TotalOrderAmount DESC;
+
+-- 34
+
+SELECT c.CustomerID, c.CompanyName, o.OrderID, SUM(od.unitPrice * od.Quantity) AS TotalsWithoutDiscount, SUM(od.unitPrice * od.Quantity * (1-od.Discount)) AS TotalsWithDiscount
+	FROM Customers c
+    JOIN Orders o
+        ON c.CustomerID = o.CustomerID
+    JOIN OrderDetails od
+        ON o.OrderID = od.OrderID
+    WHERE OrderDate LIKE '2016%'
+    GROUP BY c.CustomerID
+    HAVING (TotalsWithDiscount >= 10000)
+    ORDER BY TotalsWithDiscount DESC;
+
+
+-- 35
+
+SELECT EmployeeID, OrderID, OrderDate
+	FROM orders
+    WHERE OrderDate = LAST_DAY(SUBSTR(OrderDate, 1, 10))
+    ORDER BY EmployeeID;
+
+
+-- 36
+
+SELECT OrderID, COUNT(OrderId) AS TotalOrderDetails
+	FROM orderDetails
+    GROUP BY OrderID
+    ORDER BY TotalOrderDetails DESC
+    LIMIT 10;
+
+-- 38
+
+SELECT OrderID
+	FROM OrderDetails
+	WHERE Quantity >= 60
+    GROUP BY OrderID, Quantity
+	HAVING count(*) > 1
+    ORDER BY OrderID;
+
+-- 39
+
+SELECT *
+	FROM  orderDetails
+    WHERE OrderID IN (
+		SELECT OrderID
+			FROM OrderDetails
+			WHERE Quantity >= 60
+			GROUP BY OrderID, Quantity
+			HAVING count(*) > 1
+    )
+    ORDER BY OrderID, Quantity;
+
+-- 41
+
+SELECT OrderID, DATE(OrderDate) AS OrderDate, DATE(RequiredDate) AS RequiredDate, DATE(ShippedDate) AS ShippedDate
+	FROM orders
+    WHERE DATE(ShippedDate) >= DATE(RequiredDate)
+    ORDER BY OrderID;
+
+-- 42
+
+SELECT e.EmployeeID, LastName, COUNT(OrderID) AS TotalLateOrders
+	FROM employees e
+    JOIN orders o
+		ON e.EmployeeID = o.EmployeeID
+	WHERE ShippedDate >= RequiredDate
+	GROUP BY EmployeeID
+    ORDER BY TotalLateOrders DESC;
+
+-- 52
+
+SELECT DISTINCT Country
+	FROM  customers
+UNION
+SELECT DISTINCT Country
+	FROM suppliers
+ORDER BY Country;
+
+-- 56
+
+SELECT o.CustomerID, o.OrderID AS InitialOrderID, DATE(o.orderDate) AS InitialOrderDate, od.OrderID AS NextOrderID, DATE(od.OrderDate) AS NextOrderDate, DATEDIFF(od.OrderDate, o.OrderDate) AS DaysBetweenOrders
+	FROM  customers c
+    JOIN orders o
+		ON c.CustomerID = o.CustomerID
+	JOIN orders od
+		ON o.CustomerID = od.CustomerID
+	WHERE TIMESTAMPDIFF(SECOND, o.OrderDate, od.OrderDate) BETWEEN 1 AND 432000 OR DATEDIFF(od.OrderDate, o.OrderDate) = 5 OR (o.OrderDate = od.OrderDate AND o.OrderID != od.OrderID)
+    GROUP BY o.OrderDate, od.OrderDate
+	ORDER BY c.CustomerID, InitialOrderID;
+
