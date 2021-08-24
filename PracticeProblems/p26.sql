735b3da4dffb69b4b91cce05be25cb0c4161bd07
Alex <alexbean95@gmail.com>
2021-08-24 08:15:05 -0600

    Added note regarding more efficient way to do 26 in the original problem set

--- PracticeProblems/p26.sql
+++ PracticeProblems/p26.sql
@@ -3,3 +3,18 @@ SELECT ShipCountry, AVG(CASE WHEN OrderDate LIKE '2015%' THEN Freight END) AS Av
     GROUP BY ShipCountry
     ORDER BY AverageFreight DESC
     LIMIT 3;
+
+-- Shouldn't use conditional logic withing aggrage function unless absolutely needed
+-- It's unnecessary to convert OrderDate into a string using LIKE
+-- The following answer is faster since it doesn't require converting OrderDate
+
+SELECT
+    ShipCountry,
+    AVG(freight) AS AverageFreight
+FROM Orders
+WHERE
+    OrderDate >= '2015-01-01'
+    AND OrderDate  < '2016-01-01'
+GROUP BY ShipCountry
+ORDER BY AverageFreight DESC
+LIMIT 3;
