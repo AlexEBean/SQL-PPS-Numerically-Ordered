866836afdc24d4bea181e09e9952ecb24308e5d4
Alex <alexbean95@gmail.com>
2021-08-24 09:15:06 -0600

    Modified 32 to be more explicit in GROUP BY

--- PracticeProblems.sql
+++ PracticeProblems.sql
@@ -225,7 +225,7 @@ SELECT c.CustomerID, c.CompanyName, o.OrderID, SUM(od.unitPrice * od.Quantity) A
     JOIN OrderDetails od
         ON o.OrderID = od.OrderID
     WHERE OrderDate LIKE '2016%'
-    GROUP BY o.OrderID
+    GROUP BY c.CustomerID, c.CompanyName, o.OrderID
     HAVING (TotalOrderAmount >= 10000)
     ORDER BY TotalOrderAmount DESC;
 
