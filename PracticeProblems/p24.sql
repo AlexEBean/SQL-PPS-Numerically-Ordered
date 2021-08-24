6a8afc0465a43cf6b4758919e7536bf9174222c4
Alex <alexbean95@gmail.com>
2021-08-24 08:07:17 -0600

    included more explicit way of completing p24

--- PracticeProblems/p24.sql
+++ PracticeProblems/p24.sql
@@ -1,3 +1,28 @@
 SELECT CustomerID, CompanyName, Region
 	FROM customers
     ORDER BY Region IS NULL, region, CustomerID;
+
+-- More explicit way of writing this
+Select
+
+  CustomerID
+
+  ,CompanyName
+
+ ,Region
+
+From Customers
+
+ Order By
+
+  Case
+
+    when Region is null then 1
+
+   else 0
+
+  End
+
+ ,Region
+
+ ,CustomerID
