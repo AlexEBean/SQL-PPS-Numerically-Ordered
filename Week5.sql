49e13f218e15de4d5a16b96a67f80dd0783400b2
Alex <alexbean95@gmail.com>
2021-09-09 14:07:23 -0600

    Finished Week 5

--- Week5.sql
+++ Week5.sql
@@ -1,38 +1,49 @@
 -- 1
 
-
+-- Every row in a table will have a primary key that is unique to that one row.  
+-- It would also typically be used to join other tables.
 
 -- 2
 
-
+-- DATE
 
 -- 3
 
-
+-- VARCHAR(1)
 
 -- 4
 
-
+-- It's probably used for Home Address, Business Address, etc.
+-- VARCHAR(20) should work in this case.
 
 -- 5
 
-
+-- VARCHAR(20)
 
 -- 6
 
-
+-- VARCHAR(100)
 
 -- 7
 
-
+ALTER TABLE Employee
+ADD COLUMN ReportsTo INT NOT NULL;
 
 -- 8
 
-
+ALTER TABLE Employee
+ADD COLUMN Level INT NOT NULL;
 
 -- 9
 
-
+ALTER TABLE Employee
+ADD COLUMN Employee_picture VARCHAR(3000) NOT NULL;
 
 -- 10
 
+ALTER TABLE Employee
+ADD COLUMN ModifiedDate DATE NOT NULL;
+
+-- 11
+
+-- VARCHAR(2)
