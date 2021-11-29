b4b4e4dda8387c00f0e35fb5e777592af609de95
Alex Bean <alexelijahb@gmail.com>
2021-11-28 17:43:50 -0700

    More review.  Learned a bit about performance aspects: :

---
+++ UnusedProblemsForPractice/m34.sql
@@ -0,0 +1,14 @@
+-- Used this command to check how much Innodb_buffer_pool_read_requests in each solution
+
+SHOW STATUS LIKE 'Innodb_buffer_pool_read_requests';
+
+-- Solution 1 goes up 68
+
+-- Solution 2 goes up 68
+
+-- Solution 3 goes up 136
+
+-- Solution 4 goes up 29
+
+
+-- Does that mean solution 3 or 4 is the worst?  I need to look into this more
